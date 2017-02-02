35f0a2ce1152ea0f0618d9ad1f8ac6a40af9b6f2
Robert Rees <shudderfix@gmail.com>
2017-02-02 15:16:51 +0000

    Introduces a map function but transforms to upppercase

--- rot13/src/rot13.go
+++ rot13/src/rot13.go
@@ -3,13 +3,22 @@ package main
 import "fmt"
 import "bufio"
 import "os"
+import "strings"
+import "unicode"
 
 func main() {
-  fmt.Println("Hello from the encoder")
 
   reader := bufio.NewReader(os.Stdin)
 
   text, _ := reader.ReadString('\n')
 
-  fmt.Println(text)
+  encoded := strings.Map(rot13, text)
+
+
+  fmt.Println(encoded)
+
+}
+
+func rot13(r rune) rune {
+  return unicode.ToUpper(r)
 }
